861311292859b459a8f6a8592888c3b56cc480ed
Tyler Lee <witlee@ucsc.edu>
2022-10-16 23:28:41 -0700

    width, height ratio

--- knittes<3/script.js
+++ knittes<3/script.js
@@ -5,3 +5,11 @@ function n_rows_update() {
 function n_cols_update() {
     console.log(document.getElementById("n_cols").value);
 }
+
+function width_update() {
+    console.log(document.getElementById("width").value);
+}
+
+function height_ratio_update() {
+    console.log(document.getElementById("height_ratio").value);
+}
